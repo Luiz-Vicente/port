ce8f1638e3f51c07f0f433f6749289eba349fd61
Luiz Vicente <dev.vicente.contato@gmail.com>
2024-03-25 12:05:18 -0300

    feat: adds vercel analytics

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Analytics
+import { Analytics } from "@vercel/analytics/react";
+
 // routes
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
@@ -12,6 +15,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById("root-react")!).render(
 	<React.StrictMode>
 		<ChakraProvider theme={customTheme}>
+			<Analytics />
 			<RouterProvider router={router} />
 		</ChakraProvider>
 	</React.StrictMode>
